1729042d1d1a17a76d9daff55904f2d279d735e6
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-04 19:30:12 +0000

    some basic CSS styles applied

--- public/app.js
+++ public/app.js
@@ -10,8 +10,13 @@ window.onload = function(){
   cat2 = new Cat("Barnaby", "Tuna", "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg");
   cat3 = new Cat("Max", "Whiskas Temptations", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg");
   cat4 = new Cat("Hobbes", "Tuna", "https://i.imgur.com/XoDWKiz.jpg");
+  cat5 = new Cat("Thiccie", "Everything", "https://i.redd.it/n3ktuqp02poz.jpg");
+  cat6 = new Cat("Dm9 Cat", "Jazz Cabbage", "https://i.redd.it/yc5hd1aj39uz.jpg");
+  cat7 = new Cat("Frank", "TGI Frankdays", "https://i.imgur.com/e0CFglM.jpg");
+  cat8 = new Cat("Thicc Boye", "Marshmallows", "https://i.redd.it/prcnufbgv0ky.jpg");
+  cat9 = new Cat("Frank, again", "Anything", "https://i.imgur.com/GdHKFFr.jpg");
 
-  var cats = [cat1, cat2, cat3, cat4];
+  var cats = [cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9];
 
   for (var cat of cats) {
     newCat(cat.name, cat.favFood, cat.imgLink);
@@ -32,7 +37,7 @@ var newCat = function(name, favFood, imgLink){
 
   var image = document.createElement('img');
   image.setAttribute('src', imgLink);
-  image.setAttribute('width', '500px');
+  image.setAttribute('width', '250px');
 
   imageListItem.appendChild(image)
   newCatListItem.appendChild(nameListItem);
@@ -40,7 +45,3 @@ var newCat = function(name, favFood, imgLink){
   newCatListItem.appendChild(imageListItem);
   catSection.appendChild(newCatListItem);
 };
-
-// var addCat = function(name, favFood, imgLink) {
-//   var cat = newCat(name, favFood, imgLink);
-// };
